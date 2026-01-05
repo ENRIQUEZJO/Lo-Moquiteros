c6eafa0f6629c6f74ce17b90578d0dc9b30038fe
ENRIQUEZJO <166477080+ENRIQUEZJO@users.noreply.github.com>
2026-01-05 01:43:18 -0400

    Merge branch 'JosEnriquezRepo' of https://github.com/juniorvaleraofficial/Lo-Moquiteros

---
+++ Lo-Moquiteros/Carpetas-jose-enriquez/funciones.js
@@ -0,0 +1,12 @@
+//PRIMERA FUNCION QUE YO REALICE
+
+function sumar(a, b){
+    let resultado = a + b;
+    return resultado;
+}
+
+let a=2, b=8, c;
+
+c = sumar (a, b);
+
+console.log(`Resultado sumar:`,c)
